56075fb21c680ec0e530fecdedb56fe2023efc03
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-04-28 13:46:47 -0500

    Fixing some tasks

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,10 +3,10 @@ def uppercase(s):
     l = ""
     for i in range(0, len(s)):
         if ord(s[i]) >= 97 and ord(s[i]) <= 122:
-            l = chr(ord(s[i]) - 32)
+            l = ord(s[i]) - 32
         else:
-            l = s[i]
+            l = ord(s[i])
         if i + 1 < len(s):
-            print(l, end="")
+            print("{:c}".format(l), end="")
         else:
-            print(l)
+            print("{:c}".format(l))
