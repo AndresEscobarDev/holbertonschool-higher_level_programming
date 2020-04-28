d5cd5f6cb676879f55e21b3e126e2c30670d85af
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-04-28 14:09:39 -0500

    fix 9 task

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 def print_last_digit(number):
-    if number < 0:
-        number = number * -1
-    n = number % 10
+    n = abs(number) % 10
     print("{:d}".format(n), end="")
     return n
