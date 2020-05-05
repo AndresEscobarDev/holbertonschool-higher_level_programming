84cb277a5ad7553482a75e985c863c8369ae2f14
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-05-05 15:29:57 -0500

    Fixing some mandatory tasks

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,34 +1,37 @@
 #include "lists.h"
-#include <stdlib.h>
+#include <stdio.h>
 /**
- * is_palindrome - Function that evaluates if a linked list is palindrome or not
- * 
+ * is_palindrome - Function that evaluates if a linked
+ * list is palindrome or not
+ * @head: header of a linked list
+ * Return: 1 if is palidrome 0 if not
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *current = NULL;
-    int i, j;
-    char *s = NULL;
+	listint_t *current = NULL;
+	int i = 5, j = 0;
+	int *s = NULL;
 
-    if (!current)
-        return (1);
-    current = *head;
-    for (i = 0; current; i++)
-        current = current->next;
-    current = *head;
-    s = malloc(sizeof(char) * (i + 1));
-    for (i = 0; current; i++)
-    {
-        s[i] = current->n;
-        current = current->next;
-    }
-    for (j = 0, i--; i > j; i--, j++)
-    {
-        if (s[i] != s[j])
-        {
-            free(s);
-            return (0);
-        }
-    }
-    return (1);
+	if (!*head)
+		return (1);
+	current = *head;
+	for (i = 0; current; i++)
+		current = current->next;
+	current = *head;
+	s = malloc(sizeof(int) * (i + 1));
+	for (i = 0; current; i++)
+	{
+		s[i] = current->n;
+		current = current->next;
+	}
+	for (j = 0, i--; i > j; i--, j++)
+	{
+		if (s[i] != s[j])
+		{
+			free(s);
+			return (0);
+		}
+	}
+	free(s);
+	return (1);
 }
