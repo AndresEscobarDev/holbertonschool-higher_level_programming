7c62797c053587db846ed7691c13c7bffdeee23e
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-05-05 19:26:41 -0500

    Fixed 13 task

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,23 @@
 #include "lists.h"
 /**
+ * add_nodeint - adds a new node at the beginning of a listint_t list.
+ * @head: Head of the listint.
+ * @n: new element.
+ * Return: A pointer to the new element.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (0);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+/**
  * is_palindrome - Function that evaluates if a linked
  * list is palindrome or not
  * @head: header of a linked list
@@ -7,24 +25,26 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *current = NULL, *end = NULL;
-	int i, j;
+	listint_t *current = NULL, *new = NULL;
 
 	if (!*head || !head[0]->next)
 		return (1);
 	current = *head;
-	for (i = 0; current; i++)
-		current = current->next;
-	current = *head;
-	for (j = 0, i--; current; i++)
+	while (current)
 	{
-		end = current;
-		for (j = 0; j < i; j++)
-			end = end->next;
-		if (end->n != current->n)
-			return (0);
-		i -= 3;
+		new = add_nodeint(&new, current->n);
 		current = current->next;
+	}
+	current = *head;
+	while (current)
+	{
+		if (current->n != new->n)
+		{
+			free_listint(new);
+			return (0);
+		}
+		current = current->next;
+		new = new->next;
 	}
 	return (1);
 }
