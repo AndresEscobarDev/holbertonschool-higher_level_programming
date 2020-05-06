37a2f42912d20860e61a697720f0abbcf2481867
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-05-05 21:06:47 -0500

    Fixed task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,6 +3,7 @@
  * be_or_not_be - Function that evaluates if a linked
  * list is palindrome or not
  * @head: header of a linked list
+ * @i: len of the list
  * Return: 1 if is palidrome 0 if not
  */
 int be_or_not_be(int i, listint_t *head)
