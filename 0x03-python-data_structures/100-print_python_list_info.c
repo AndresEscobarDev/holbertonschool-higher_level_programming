5d38ec689dd192043f91e52c23ab03c0c5fb523d
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-05-05 21:15:06 -0500

    Fixing advance task and last mandaroty task

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -3,14 +3,11 @@
 
 void print_python_list_info(PyObject *p)
 {
-    PyListObject *list;
-    unsigned long int size = PyList_Size(p);
-    printf("[*] Size of the Python List = %ld\n", size);
-    printf("[*] Allocated = %ld\n", list->allocated)
-    for (i = 0; i < size; i++)
-    {
-        list = (PyList_GET_ITEM(p, i))->ob_item[i]->tp_name;
-        printf("Element %d: %s\n", i, list);
-    }
-    
+	PyListObject *list = (PyObject *)p;
+
+	unsigned long int size = PyList_Size(p);
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", list->allocated)
+	for (i = 0; i < size; i++)
+		printf("Element %d: %s\n", i, PyList_GET_ITEM(p, i)->ob_item[i]->tp_name);
 }
