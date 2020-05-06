21f32abbf1b83e1e50840bf838ca712c350ea2fc
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-05-05 21:24:34 -0500

    Fixed 13 task

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,30 +1,5 @@
 #include "lists.h"
-/**
- * be_or_not_be - Function that evaluates if a linked
- * list is palindrome or not
- * @head: header of a linked list
- * @i: len of the list
- * Return: 1 if is palidrome 0 if not
- */
-int be_or_not_be(int i, listint_t *head)
-{
-	int s[i], j = 0;
 
-	while (head)
-	{
-		s[j++] = head->n;
-		head = head->next;
-	}
-	i = 0;
-	j--;
-	while (i < j)
-	{
-
-		if (s[i++] != s[j--])
-			return (0);
-	}
-	return (1);
-}
 /**
  * is_palindrome - Function that evaluates if a linked
  * list is palindrome or not
@@ -34,12 +9,22 @@ int be_or_not_be(int i, listint_t *head)
 int is_palindrome(listint_t **head)
 {
 	listint_t *current = NULL;
-	int i = 0;
+	int i = 0, j = 0;
 
 	if (!*head || !head[0]->next)
 		return (1);
 	current = *head;
 	while (current)
 		current = current->next, i++;
-	return (be_or_not_be(i, *head));
+	current = *head;
+	int s[i];
+	while (current)
+		s[j++] = current->n, current = current->next;
+	i = 0;
+	j--;
+	while (i < j)
+		if (s[i++] != s[j--])
+			return (0);
+	return (1);
+
 }
