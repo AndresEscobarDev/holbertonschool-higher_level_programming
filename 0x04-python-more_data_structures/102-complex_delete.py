d36fe6597a9a24266bf8596302ffc587b4394588
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-05-07 15:48:06 -0500

    Advance task 102

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if not a_dictionary:
+        return None
+    switch = True
+    while switch:
+        c = 0
+        for i, j in a_dictionary.items():
+            if j == value:
+                del a_dictionary[i]
+                break
+            if c == len(a_dictionary) - 1:
+                switch = False
+            c += 1
+    return a_dictionary
