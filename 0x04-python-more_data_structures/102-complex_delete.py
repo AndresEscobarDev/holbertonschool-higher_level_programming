e8a79bbecea55c210b91df024957a890d1f333b9
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-05-07 16:19:42 -0500

    Fix task 102

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if not a_dictionary:
-        return None
-    if not value:
-        return a_dictionary
-    switch = True
-    while switch:
-        c = 0
-        for i, j in a_dictionary.items():
-            if j == value:
-                del a_dictionary[i]
-                break
-            if c == len(a_dictionary) - 1:
-                switch = False
-            c += 1
+    for i in list(a_dictionary):
+        if a_dictionary[i] == value:
+            del a_dictionary[i]
     return a_dictionary
