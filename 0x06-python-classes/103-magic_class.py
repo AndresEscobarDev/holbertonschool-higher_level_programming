1d38fd6829d59a0486fa71bd6206ef2e10bde3a3
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-05-19 17:23:07 -0500

    Last advance task

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
+"""Magic Module"""
 import math
+
+
 class MagicClass():
+    """MagicClass.
+    Public instance attribute: _MagicClass__radius.
+    Instantiation with optional radius.
+    Public instance method: def area(self).
+    Public instance method: def circumference(self).
+    """
     def __init__(self, radius=0):
+        """Constructor"""
         self._MagicClass__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         _MagicClass__radius = radius
+
     def area(self):
+        """returns the current circumference area"""
         return self._MagicClass__radius ** 2 * math.pi
+
     def circumference(self):
+        """returns circumference"""
         return 2 * math.pi * self._MagicClass__radius
