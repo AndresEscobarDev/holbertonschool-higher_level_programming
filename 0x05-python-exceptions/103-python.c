843637d5f09e3e5d468bbe798cbb496ef6929221
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-05-18 19:52:20 -0500

    Last advance task

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,69 @@
+#include <Python.h>
+#include <stdio.h>
+#include <string.h>
+
+void print_python_bytes(PyObject *p)
+{
+	char *s = NULL;
+	long int len = 0, i;  
+
+	printf("[.] bytes object info\n");
+	if(!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+	PyBytes_AsStringAndSize(p, &s, &len);
+	printf("  size: %ld\n", len);
+	printf("  trying string: %s\n", s);
+	if (len < 10)
+		printf("  first %ld bytes:", len + 1);
+	else
+		printf("  first 10 bytes:");
+	for (i = 0; i <= len && i < 10; i++)
+		printf(" %02hhx", s[i]);
+	printf("\n");
+}
+
+void print_python_float(PyObject *p)
+{
+	double num = 0;
+
+	printf("[.] float object info\n");
+	if(!PyFloat_Check(p))
+	{
+		printf("  [ERROR] Invalid Float Object\n");
+		return;
+	}
+	num = PyFloat_AsDouble(p);
+	if (num - (int)num == 0)
+		printf("value: %.1f\n", num);
+	else
+		printf("value: %.16g\n", num);
+}
+
+void print_python_list(PyObject *p)
+{
+	long int len = 0, i;
+	PyListObject *py_item = (PyListObject *)p;
+	const char *type;
+
+	printf("[*] Python list info\n");
+	if(!PyList_Check(p))
+	{
+		printf("  [ERROR] Invalid List Object\n");
+		return;
+	}
+	len = ((PyVarObject *)p)->ob_size;
+	printf("[*] Size of the Python List = %ld\n", len);
+	printf("[*] Allocated = %ld\n", py_item->allocated);
+	for (i = 0; i < len; i++)
+	{
+		type = (py_item->ob_item[i])->ob_type->tp_name;
+		printf("Element %ld: %s\n", i, type);
+		if (!strcmp(type, "bytes"))
+			print_python_bytes(py_item->ob_item[i]);
+		if (!strcmp(type, "float"))
+			print_python_float(py_item->ob_item[i]);
+	}
+}
