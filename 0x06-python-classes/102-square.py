eff9444c031ed24cf55c6a186902ceb69584d196
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-05-19 16:48:03 -0500

    Advance task 102

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -14,24 +14,6 @@ class Square():
         """Constructor"""
         self.size = size
 
-    def area(self):
-        """returns the current square area"""
-        return self.__size ** 2
-
-    @property
-    def size(self):
-        """retrieves the size"""
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """sets the size"""
-        if type(value) != int:
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
-
     def __eq__(self, next):
         """Equal."""
         return self.__size == next
@@ -55,3 +37,21 @@ class Square():
     def __ge__(self, next):
         """Greater or equal than."""
         return self.__size >= next
+
+    def area(self):
+        """returns the current square area"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """retrieves the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets the size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
