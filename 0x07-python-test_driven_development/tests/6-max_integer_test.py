3df1643f243c14c5044ae7d204581f38ac20e4ca
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-05-23 01:06:59 -0500

    Fixing task 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -13,3 +13,6 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([-56, 1, -564911]), 1)
         self.assertEqual(max_integer([]), None)
         self.assertEqual(max_integer(), None)
+
+if __name__ == "__main__":
+    unittest.main()
