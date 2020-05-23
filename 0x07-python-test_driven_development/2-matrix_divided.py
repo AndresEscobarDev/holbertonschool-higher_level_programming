bfc86fd911ac16d156fe42a6f09a688729f54e68
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-05-23 01:03:14 -0500

    Fixing task 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,19 +4,20 @@
 
 def matrix_divided(m, div):
     """ Function that divides all elements of a matrix."""
+    s = "matrix must be a matrix (list of lists) of integers/floats"
     if type(m) != list or len(m) == 0 or type(m[0]) != list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(s)
     l = len(m[0])
     if l == 0:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(s)
     for i in m:
         if type(i) != list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(s)
         if len(i) != l:
             raise TypeError("Each row of the matrix must have the same size")
         for j in i:
             if type(j) != float and type(j) != int:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(s)
     if type(div) != int and type(div) != float:
         raise TypeError("div must be a number")
     if div == 0:
