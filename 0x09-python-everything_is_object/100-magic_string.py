8b7edd1ab7504be9e8adebe5eda30314db5a7da3
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-05-26 18:26:25 -0500

    Some Fixes

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Function that print Holberton one more time every time that is invoked"""
+"""Function that print Holberton one more
+time every time that is invoked
+"""
 
 
-def magic_string(i = [-1]):
+def magic_string(i=[-1]):
     i[0] += 1
     return "Holberton" + (", Holberton" * i[0])
--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-"""Copy Module"""
-
-
 def copy_list(l):
-    """Function that copy a list"""
     return l[:]
