ddf8c57ab8ed523be27800f39300f008c1c19cb3
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-05-26 18:30:59 -0500

    task 100 fixed

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-"""Function that print Holberton one more
-time every time that is invoked
-"""
-
-
 def magic_string(i=[-1]):
     i[0] += 1
     return "Holberton" + (", Holberton" * i[0])
