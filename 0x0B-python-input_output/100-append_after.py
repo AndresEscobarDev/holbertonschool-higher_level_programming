3671161899b449e95dd3a1242e12ec7bffe18226
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-06-03 22:03:15 -0500

    First advance task

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,6 +3,10 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """ function that inserts a line of
+    text to a file, after each line
+    containing a specific string
+    """
     with open(filename, 'r') as f:
         text = f.readlines()
 
