1a0857aea3ba7f79be4a0da1f95977c3e1a562f5
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-07-17 00:14:55 +0000

    Fixed task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server.
--- lists all privileges of the MySQL user user_0d_1.
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- lists all privileges of the MySQL user user_0d_2.
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
