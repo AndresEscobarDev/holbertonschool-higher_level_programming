bdbd9ce35ae318985884c15947eda03f6490ae44
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-09-16 05:01:31 +0000

    Fixed tasks

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 let n = 0;
 let m = process.argv[2];
-for (let i = 3; i < process.argv.length; i++){
+for (let i = 3; i < process.argv.length; i++) {
     if (process.argv[i] > m){
         n = m;
         m = process.argv[i];
