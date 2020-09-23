da43eea08b1ddb16082f0791949d2a3fd282cd95
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-09-23 15:50:25 +0000

    Fixed task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,2 @@
 #!/usr/bin/node
-let n = parseInt(process.argv[3]);
-if (process.argv.length > 3) {
-  let m = parseInt(process.argv[2]);
-  for (let i = 3; i < process.argv.length; i++) {
-      if (parseInt(process.argv[i]) > m) {
-        n = m;
-        m = parseInt(process.argv[i]);
-    }
-  }
-  console.log(n);
-} else {
-  console.log(0);
-}
+console.log((process.argv.length < 3) ? 0 : process.argv.slice(2).sort ((a, b) => b - a)[1])
