6e7b9b4b07d269ea91e9deeb2cd19c51c6058e40
Andres Felipe Escobar Cardona <felipeescobar15@gmail.com>
2020-09-23 14:19:49 +0000

    Fixed tasks 2, 3, 5, 6, 7, 8, 9, 10, 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,9 +2,9 @@
 let n = 0;
 let m = process.argv[2];
 for (let i = 3; i < process.argv.length; i++) {
-    if (process.argv[i] > m){
-        n = m;
-        m = process.argv[i];
-    }
+  if (process.argv[i] > m) {
+    n = m;
+    m = process.argv[i];
+  }
 }
 console.log(n);
